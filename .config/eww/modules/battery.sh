7632b3a3b0032ce863e17674338f0a104fd0dc76
niceduckdev <66257784+niceduckdev@users.noreply.github.com>
2024-09-08 13:37:59 +0200

    Update battery.sh

--- .config/eww/modules/battery.sh
+++ .config/eww/modules/battery.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "state" | awk '{print $2}')
 percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage" | awk '{print $2}' | sed 's/%//')

3b18017196639468deb5c005ea054dcda56a3256
niceduckdev <bottykaj@gmail.com>
2024-12-16 15:26:26 +0100

    AA

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,6 +15,7 @@
 	networking.hostName = "laptoep";
 	networking.networkmanager.enable = true;
 	hardware.bluetooth.enable = false;
+	environment.etc."issue".text = "beep boop\n";
 
 	#############################
 	###    TIME & KEYBOARD    ###
@@ -29,33 +30,36 @@
 	services.xserver = {
   		enable = true;
 		displayManager.startx.enable = true;
-        desktopManager.xterm.enable = false;
 		windowManager.i3 = {
 			enable = true;
 			extraPackages = with pkgs; [
 				rofi i3status i3lock xss-lock
-				picom xwallpaper alacritty
-				maim xclip eww jq brightnessctl
+				picom xwallpaper alacritty eza
+				maim xclip eww jq brightnessctl flatpak-builder
 			];
 		};
   		xkb = {
     		layout = "be";
     		variant = "";
   		};
+		excludePackages = [ pkgs.xterm ];
   	};
 
 	######################
 	###    SERVICES    ###
 	######################
 	services.tlp.enable = true;
-	services.printing.enable = true;
+	services.printing.enable = false;
 	services.flatpak.enable = true;
 	hardware.pulseaudio.enable = true;
 	security.rtkit.enable = true;
 	virtualisation.virtualbox.host.enable = true;
 
-	xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-kde ];
 	xdg.portal.enable = true;
+	xdg.portal.extraPortals = with pkgs; [
+    	xdg-desktop-portal-kde
+  	];
+	xdg.portal.config.common.default = "*";
 
 	###################
 	###    USERS    ###
@@ -74,14 +78,13 @@
   	programs.firefox.enable = true;
   	programs.zsh.enable = true;
   	environment.systemPackages = with pkgs; [
-		git zsh neovim killall eza pfetch gcc
-		discord zip unzip papirus-icon-theme fzf
-		python3 vscodium nodejs spotify
+		git zsh neovim killall gcc
+		zip unzip python3 nodejs
 		
 		obsidian jetbrains.rider ciscoPacketTracer8
 		dotnetCorePackages.sdk_8_0_2xx
 		mono umlet ipcalc jetbrains.jdk
-  	]; 
+  	];
   	fonts.packages = with pkgs; [
 		fira-code-nerdfont
 	];
---
+++ scripts/restart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+i3-msg restart
+eww reload

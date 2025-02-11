282e1bce437f07750d3d110e1a62c911a1667655
niceduckdev <bottykaj@gmail.com>
2025-02-11 16:29:55 +0100

    telescope theme

--- nvim/lua/plugins/file-browser.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	config = function()
-		local telescope = require("telescope.builtin")
-		vim.keymap.set("n", "<C-b>", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-	end
-}
